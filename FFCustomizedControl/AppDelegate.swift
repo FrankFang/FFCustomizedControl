6cd6c0fba647a22b4d7f90fa0df7b893e1726f75
Frank Fang <frankfang1990@gmail.com>
2014-06-11 10:45:00 +0800

    Initial Commit

---
+++ FFCustomizedControl/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  FFCustomizedControl
+//
+//  Created by FrankFang on 14-6-11.
+//  Copyright (c) 2014年 FrankFang. All rights reserved.
+//
+
+import Cocoa
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+                            
+    @IBOutlet var window: NSWindow
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification?) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification?) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ FFCustomizedControl/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  FFCustomizedControl
+//
+//  Created by FrankFang on 14-6-11.
+//  Copyright (c) 2014年 FrankFang. All rights reserved.
+//
+
+import Cocoa
+
+NSApplicationMain(C_ARGC, C_ARGV)
