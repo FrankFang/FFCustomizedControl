26081141cc36f579c6310f686e907ac1dbbfb5f8
Frank Fang <frankfang1990@gmail.com>
2014-07-02 09:52:11 +0800

    add knob node.

--- FFCustomizedControl/FFSwitch.swift
+++ FFCustomizedControl/FFSwitch.swift
@@ -10,13 +10,12 @@ import Cocoa
 import QuartzCore
 
 class FFSwitch: NSControl {
+    
 
     init(frame: NSRect) {
         super.init(frame: frame)
+        self.setUp()
         // Initialization code here.
-    }
-    init(coder: NSCoder!){
-        super.init(coder: coder)
     }
 
     override func drawRect(dirtyRect: NSRect) {
@@ -30,17 +29,85 @@ class FFSwitch: NSControl {
         self.setUpLayers()
     }
     
+    var bg = CALayer()
+    var knob = CALayer()
+    var knobBgColor = NSColor(calibratedWhite:1.0,alpha:1.0)
+    var inner = CALayer()
+    
     func setUpLayers(){
         var rootLayer = CALayer()
         
         self.layer = rootLayer
         self.wantsLayer = true
         
-        var bgLayer = CALayer()
-        bgLayer.autoresizingMask = CAAutoresizingMask.LayerWidthSizable | CAAutoresizingMask.LayerHeightSizable
-        bgLayer.bounds = rootLayer.bounds
-        bgLayer.anchorPoint = CGPoint(x:0,y:0)
+        // add background
+        
+        bg.autoresizingMask = CAAutoresizingMask.LayerWidthSizable | CAAutoresizingMask.LayerHeightSizable
+        bg.bounds = rootLayer.bounds
+        bg.anchorPoint = CGPoint(x:0,y:0)
+        bg.borderWidth = 1.0
+        rootLayer.addSublayer(bg)
+        
+        // add knob
+        knob.frame = self.getKnobRect()
+        knob.autoresizingMask = CAAutoresizingMask.LayerHeightSizable
+        knob.backgroundColor = knobBgColor.CGColor
+        knob.shadowColor = NSColor.blackColor().CGColor
+        knob.shadowOffset = CGSize(width:0.0,height:-2.0)
+        knob.shadowRadius = 1.0
+        knob.shadowOpacity = 0.3
+        rootLayer.addSublayer(knob)
+        
+        inner.frame = knob.bounds
+        inner.autoresizingMask = CAAutoresizingMask.LayerWidthSizable | CAAutoresizingMask.LayerHeightSizable
+        inner.shadowColor = NSColor.blackColor().CGColor
+        inner.shadowOffset = CGSize(width: 0.0, height: 0.0)
+        inner.backgroundColor = NSColor.whiteColor().CGColor
+        inner.shadowRadius = 1.0
+        inner.shadowOpacity = 0.35
+        knob.addSublayer(inner)
+        
+        self.reloadLayerSize()
+        
         
     }
     
+    func setUp(){
+        self.enabled = true
+        setUpLayers()
+    }
+    
+    var isActive = false
+    var ratio = 1.0
+    var bRatio = 1.0
+    
+    func getKnobRect() -> CGRect{
+        var height = bg.bounds.size.height - 2 * 1
+        var width = bg.bounds.size.width - 2 * 1
+        var x = 1.0
+        var y = 1.0
+        var rect = CGRect()
+        rect.size.width = width
+        rect.size.height = height
+        rect.origin.x = x
+        rect.origin.y = y
+        return rect
+    }
+    
+    func reloadLayerSize() {
+        CATransaction.begin()
+        CATransaction.setDisableActions(true)
+        bg.cornerRadius = bg.bounds.size.height / 2
+        knob.cornerRadius = knob.bounds.size.height / 2
+        inner.cornerRadius = knob.bounds.size.height / 2
+        CATransaction.commit()
+    }
+    
+    func reloadLayer(){
+    }
+    
+    
+    
+    
 }
+
